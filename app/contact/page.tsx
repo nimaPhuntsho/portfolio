f8dc767190070368993dad3a50a2d842c0edec6a
nimaPhuntsho <nimafunso@gmail.com>
2025-04-16 20:05:38 +1000

    build fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import ContactForm from "../components/ContactForm";
-import Footer from "../components/Footer";
 export const metadata: Metadata = {
   title: "Contact 📞",
   description: "Portfolio designed and developed by Nima",
